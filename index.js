a7c6c15a381c738e18ac79ec8e836fcc1c5d4712
sohan-99 <sohan75632@gmail.com>
2024-05-22 18:17:02 +0600

    Connect to local mongodb

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 6001;
+const URI = process.env.MongoDBURI;
+
+// connect to mongoDB
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
+    console.log("Error: ", error);
+}
 app.get('/', (req, res) => {
-  res.send('Hello gelo!')
+  res.send('Hello World!')
 })
-
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+// defining routes
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
