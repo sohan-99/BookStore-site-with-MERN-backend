c05f0fbfe161188c5bae72e807abf1775b1de3ba
sohan-99 <sohan75632@gmail.com>
2024-06-05 13:46:58 +0600

    login user functionalty [DONE]

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,4 +1,4 @@
-import User from "../model/user.model.js"; 
+import User from "../model/user.model.js";
 import bcrypt from "bcrypt";
 export const signup = async (req, res) => {
   try {
@@ -8,18 +8,42 @@ export const signup = async (req, res) => {
     if (user) {
       return res.status(400).json({ message: "User already exists" });
     }
-const hashpassword = await bcrypt.hash(password,10);
+    const hashpassword = await bcrypt.hash(password, 10);
     const createdUser = new User({
-      fullname:fullname,
-      email:email,
+      fullname: fullname,
+      email: email,
       password: hashpassword,
     });
 
     await createdUser.save();
 
     res.status(201).json({ message: "User created successfully" });
-  }  catch (error) {
+  } catch (error) {
     console.log("Error: " + error.message);
     res.status(500).json({ message: "Internal server error" });
-}
-}
+  }
+};
+// login the user
+
+export const login = async(req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!user || !isMatch) {
+            return res.status(400).json({ message: "Invalid username or password" });
+        } else {
+            res.status(200).json({
+                message: "Login successful",
+                user: {
+                    _id: user._id,
+                    fullname: user.fullname,
+                    email: user.email,
+                },
+            });
+        }
+    } catch (error) {
+        console.log("Error: " + error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
