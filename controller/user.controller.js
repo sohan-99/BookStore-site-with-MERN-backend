87f8dd9fd52a0d82bb3891bb3191080f22c62563
sohan-99 <sohan75632@gmail.com>
2024-06-05 13:59:39 +0600

    some bug fixed

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -24,26 +24,22 @@ export const signup = async (req, res) => {
   }
 };
 // login the user
-
-export const login = async(req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!user || !isMatch) {
-            return res.status(400).json({ message: "Invalid username or password" });
-        } else {
-            res.status(200).json({
-                message: "Login successful",
-                user: {
-                    _id: user._id,
-                    fullname: user.fullname,
-                    email: user.email,
-                },
-            });
-        }
-    } catch (error) {
-        console.log("Error: " + error.message);
-        res.status(500).json({ message: "Internal server error" });
-    }
-};
+export const login = async (req, res) => {
+try {
+  const {email, password} = req.body;
+  const user = await User.findOne({ email });
+  const isValid =await bcrypt.compare(password, user.password);
+  if(!user || !isValid) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }else{
+    res.status(200).json({ message: "User logged in successfully",user:{
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email
+    }})
+  }
+}  catch (error) {
+  console.log("Error: " + error.message);
+  res.status(500).json({ message: "Internal server error!" });
+}
+}
